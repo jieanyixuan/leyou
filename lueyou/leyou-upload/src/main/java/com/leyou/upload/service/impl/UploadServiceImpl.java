ae6aeaddc749554948d0bfecf91f41c26ce5be02
王俊杰 <2629340922@qq.com>
2020-03-15 11:56:18 +0800

    将文件上传服务器改为论文fastDFS分布式文件系统

--- lueyou/leyou-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
+++ lueyou/leyou-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
@@ -1,11 +1,15 @@
 package com.leyou.upload.service.impl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.upload.service.UploadService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -14,7 +18,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * @author 王俊杰
@@ -28,6 +31,10 @@ public class UploadServiceImpl implements UploadService {
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif");
    //定义日志
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
+
+    //fastFDS文件系统java客户端
+    @Autowired
+    private FastFileStorageClient fastFileStorageClient;
     /**
      * 图片上传
      *
@@ -54,11 +61,16 @@ public class UploadServiceImpl implements UploadService {
                 return null;
             }
 
+            //文件后缀
+            String suffix = StringUtils.unqualify (filename); //该方法得到最后一个点后面的内容
+
             //文件上传到文件服务器
-            file.transferTo (new File ("E:\\imageservice\\logo\\" + filename));
+            //file.transferTo (new File ("E:\\imageservice\\logo\\" + filename));
+            StorePath storePath = fastFileStorageClient.uploadFile (file.getInputStream (), file.getSize (), suffix, null);
 
             //生成url地址,返回
-            return "http://image.leyou.com/logo/" + filename;
+            //return "http://image.leyou.com/logo/" + filename;
+            return "http://image.leyou.com/" + storePath.getFullPath ();
         } catch (IOException e) {
             LOGGER.info ("服务器内部错误:{}",filename);
             e.printStackTrace ();
